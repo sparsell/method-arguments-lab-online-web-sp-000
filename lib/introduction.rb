a8d0d26d19a20f47319be31d07874254233e2a3b
Sonja Parsell <hello@sparsell.com>
2020-03-21 10:14:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
+introduction("Sonja")
 
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("Sonja")
+introduction_with_language("Sonja", "Ruby")
